431c938e972f2ae40fe0ed8ad9164c7b09a174ed
Fatih Saglam <152738963+fthsgl@users.noreply.github.com>
2025-01-13 15:15:13 +0100

    Update gluecksrad.js

--- gluecksrad.js
+++ gluecksrad.js
@@ -1,10 +1,9 @@
 document.getElementById('spin').addEventListener('click', function () {
     const items = JSON.parse(localStorage.getItem('items')) || [];
-    
-    if (items.length > 0) {
-        const randomItem = items[Math.floor(Math.random() * items.length)];
-        document.getElementById('wheel-result').innerHTML = `<h2 class="display-5 text-success">Kazanan: <strong>${randomItem}</strong></h2>`;
+    if (items.length === 0) {
+        alert('Lütfen önce item ekleyin!');
     } else {
-        alert("Önce item eklemelisiniz!");
+        const randomItem = items[Math.floor(Math.random() * items.length)];
+        document.getElementById('wheel-result').textContent = `Seçilen Item: ${randomItem}`;
     }
 });
