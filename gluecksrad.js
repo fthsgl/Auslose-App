5f68067424ce11d8132527d8852dd565577ed27e
Fatih Saglam <152738963+fthsgl@users.noreply.github.com>
2025-01-13 15:55:07 +0100

    Update gluecksrad.js

--- gluecksrad.js
+++ gluecksrad.js
@@ -3,7 +3,13 @@ document.getElementById('spin').addEventListener('click', function () {
     if (items.length === 0) {
         alert('Lütfen önce item ekleyin!');
     } else {
-        const randomItem = items[Math.floor(Math.random() * items.length)];
-        document.getElementById('wheel-result').textContent = `Seçilen Item: ${randomItem}`;
+        const wheel = document.getElementById('wheel');
+        wheel.classList.add('spin');
+
+        setTimeout(function () {
+            const randomItem = items[Math.floor(Math.random() * items.length)];
+            document.getElementById('wheel-result').textContent = `Seçilen Item: ${randomItem}`;
+            wheel.classList.remove('spin');
+        }, 3000); // 3 saniye sonra sonucu göster
     }
 });
